7111ff0389bde6a4de2a8c8d5f9c03adcb7ad88f
Ryan Simmons <simmonsrgw@gmail.com>
2020-02-18 22:07:46 -0500

    Wrote code to convert from Markdown to HTML

--- README.md
+++ README.md
@@ -5,9 +5,9 @@ A Visual Studio Code extension that converts markdown files to PDFs.
 ## Dependencies
 
 - ["Markdown All-In-One" VS Code extension by Yu Zhang](https://marketplace.visualstudio.com/items?itemName=yzhang.markdown-all-in-one)
-- ["html-pdf" Node.js module by marcbachmann](https://www.npmjs.com/package/html-pdf)
+- ["puppeteer" Node.js module](https://www.npmjs.com/package/puppeteer)
 
 ## To-Do
-- Get conversion from Markdown to HTML working
 - Get conversion from HTML to PDF working
 - Ensuring PDFs are saved in the correct location (same directory as `.md` file)
+- Deleting HTML document after PDF is created
--- extension.js
+++ extension.js
@@ -2,6 +2,12 @@
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
+//Import fs module and reference it with the alias fs
+const fs = require('fs');
+
+//Import puppeteer module and reference it with the alias puppeteer
+const puppeteer = require('puppeteer');
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 
@@ -9,22 +15,39 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "markdown-to-pdf" is now active!');
-
 	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
+	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let createPDF = vscode.commands.registerCommand('markdown-to-pdf.createpdf', function () {
+		//Execute the command from Markdown All-In-One to convert Markdown to HTML
+		vscode.commands.executeCommand('markdown.extension.printToHtml');
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		//Get the path of the HTML document
+		let mdPath = vscode.window.activeTextEditor.document.fileName;
+		let htmlPath = mdPath.substring(0, mdPath.lastIndexOf(".")) + ".html";
+
+		//Get the path of the PDF document
+		let pdfPath = htmlPath.substring(0, htmlPath.lastIndexOf(".")) + ".pdf";
+
+		//Convert the HTML document to a PDF
+		try{
+			async () => {
+				const browser = await puppeteer.launch();
+				const document = await browser.newPage();
+				await document.goto("file://" + htmlPath);
+				await document.pdf({
+					path: pdfPath,
+					format: 'Letter'
+				});
+				await browser.close();
+			}
+		}
+		catch (err) {
+			vscode.window.showErrorMessage("Could not convert Markdown to PDF.");
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(createPDF);
 }
 exports.activate = activate;
 
